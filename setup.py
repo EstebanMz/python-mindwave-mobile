60da94a197ec322ebcd2261998949e9fc40bc6e7
EstebanMz <esja.mz10@gmail.com>
2024-11-10 17:49:21 -0500

    Print header when connection is succesful

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='mindwavemobile',
       version='0.200',
-      description='Small Library to access neurosky mindwave mobile functionality from python',
+      description='Small Library to access NeuroSky MindWave Mobile functionality from Python',
       url='https://github.com/robintibor/python-mindwave-mobile',
       author='Robin Tibor Schirrmeister',
       author_email='robintibor@gmail.com',
