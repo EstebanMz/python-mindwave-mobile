383c223227d1b5c0b1a7186a2f41b2dd654f0f34
EstebanMz <estebanmz@rva.lat>
2024-11-18 17:46:32 -0500

    Added the intended length and amount of tests to a volunteer

--- app/MindwaveReaderStart.py
+++ app/MindwaveReaderStart.py
@@ -2,11 +2,11 @@
 # @author: Esteban Martinez
 # @github: EstebanMz
 
-# This module reads data from the Mindwave Mobile EEG headset 
-# and stores it as a CSV file inside "output_files" folder.
-# The length of the output files depends on the argument of 
-# function writeDataPoints() that is called in the Main Execution
-# block as many times as it's needed.
+# This module reads data from the Mindwave Mobile EEG headset and stores it 
+# as a CSV file inside "output_files" folder. The length of the output files 
+# depends on the argument of function writeDataPoints() that is called 
+# in the Main Execution block as many times as it's needed.
+
 # The data saved from the sensor is: 
 # - EEG Power Bands (Delta, Theta, Low Alpha, High Alpha, 
 #   Low Beta, High Beta, Low Gamma, Mid Gamma)
@@ -23,8 +23,9 @@ from MindwaveWriteData import writeData
 import os
 
 # Length (rows) for output CSV files.
-SHORT_OUTPUT_LENGTH = 5
-LONG_OUTPUT_LENGTH = 10
+SHORT_OUTPUT_LENGTH = 120
+LONG_OUTPUT_LENGTH = 240
+
 
 
 # Returns an array with the data read from the sensor.
@@ -40,7 +41,8 @@ def getDataPoints(readingTime):
     dataHeader = f"{eegPower},raw_value,attention,meditation,blink,amount_of_noise"
     dataPointsArray = [dataHeader]
 
-    print(f"\nData reading is starting now for {readingTime} seconds!")
+    input("Press Enter key to continue...")
+    print(f"Data reading is starting now for {readingTime/60} minutes!")
 
     # Program will end after "data" array reaches "readingTime" size
     while(len(dataPointsArray) <= readingTime):
@@ -81,6 +83,7 @@ def getDataPoints(readingTime):
                 dataRow = f"{delta},{theta},{lowAlpha},{highAlpha},"\
                     f"{lowBeta},{highBeta},{lowGamma},{midGamma},"\
                     f"{rawValue},{attention},{meditation},{blink},{amountOfNoise}"
+                
                 # Adds sensor data as a new row
                 dataPointsArray.append(dataRow)
     
@@ -88,11 +91,11 @@ def getDataPoints(readingTime):
     return dataPointsArray
 
 
+
 # Inputs the DataPoints array and outputs the CSV file.
 def writeDataPoints(readingTime):
 
-    # Executes the code of getDataPoints() function 
-    # and stores the return result in data
+    # Executes getDataPoints() function and stores the return result in data
     data = getDataPoints(readingTime)
 
     # Create an instance of writeData() class
@@ -100,6 +103,7 @@ def writeDataPoints(readingTime):
 
     # Call the method to write data to CSV
     exportData.writeFile()
+
 
 
 # Main Execution block.
@@ -112,12 +116,27 @@ if __name__ == '__main__':
     # and continuously reads and prints data points.
     mindwaveDataPointReader = MindwaveDataPointReader()
     mindwaveDataPointReader.start()
-
+    
+    # Calls writeDataPoints() function, which writes the CSV file,
+    # inside this if as many times as its needed.
     if (mindwaveDataPointReader.isConnected()):
 
-        # Calls the function that writes the CSV 
-        # as many times as needed.
+        print("\nMotor imagination for left foot, short test.")
         writeDataPoints(SHORT_OUTPUT_LENGTH)
+
+        print("\nMotor imagination for right foot, short test.")
+        writeDataPoints(SHORT_OUTPUT_LENGTH)
+
+        print("\nResting, short test.")
+        writeDataPoints(SHORT_OUTPUT_LENGTH)
+
+        print("\nMotor imagination for left foot, long test.")
+        writeDataPoints(LONG_OUTPUT_LENGTH)
+
+        print("\nMotor imagination for right foot, long test.")
+        writeDataPoints(LONG_OUTPUT_LENGTH)
+
+        print("\nResting, long test.")
         writeDataPoints(LONG_OUTPUT_LENGTH)
         
     # Error message when device is not connected or couldn't be found.
