ffb1759e67bc1e5f09600c01e9a5749eba5c2872
EstebanMz <estebanmz@rva.lat>
2024-12-07 08:03:07 -0500

    Fix MindwaveDataframe not being sorted by date and add description

--- app/MindwaveDataframe.py
+++ app/MindwaveDataframe.py
@@ -1,3 +1,12 @@
+# MindwaveDataframe.py
+# @author: Esteban Martinez
+# @github: EstebanMz
+
+# Read all CSV files previously saved in "folder_path" by the MindWave device 
+# and merge them on a CSV file stored at "output_filename" after running "merge_csv_files()" function. 
+# This code also has the function "db_preprocessing()" that executes basic data cleaning routines. 
+# Run the code by toggle off comments at Export Dataframe section. 
+
 import pandas as pd
 import os
 
@@ -16,19 +25,25 @@ output_filename = folder_path + "MindwaveDB.csv"
 
 def merge_csv_files(folder_path, output_filename):
 
+    # Get all valid filenames and sort them in ascending order
     all_dataframes = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".csv") and filename not in ("history.csv", "MindwaveDB.csv"):
-            filepath = os.path.join(folder_path, filename)
-            try:
-                df = pd.read_csv(filepath)
-                all_dataframes.append(df)
-                print(f"Archivo '{filename}' cargado correctamente.")
-            except pd.errors.EmptyDataError:
-                print(f"Advertencia: El archivo '{filename}' está vacío y se omitirá.")
-            except pd.errors.ParserError:
-                print(f"Error: No se pudo analizar el archivo '{filename}'. Se omitirá.")
+    filenames = [filename for filename in os.listdir(folder_path) 
+                 if filename.endswith(".csv") and filename not in ("history.csv", "MindwaveDB.csv")]
+    filenames.sort()  
 
+    # Read the files from before and start to store their values in "all_dataframes" array
+    for filename in filenames:
+        filepath = os.path.join(folder_path, filename)
+        try:
+            df = pd.read_csv(filepath)
+            all_dataframes.append(df)
+            print(f"Archivo '{filename}' cargado correctamente.")
+        except pd.errors.EmptyDataError:
+            print(f"Advertencia: El archivo '{filename}' está vacío y se omitirá.")
+        except pd.errors.ParserError:
+            print(f"Error: No se pudo analizar el archivo '{filename}'. Se omitirá.")
+
+    # Convert "all_dataframes" array into a DataFrame and export the final result to "MindwaveDB.csv"
     if all_dataframes:
         merged_df = pd.concat(all_dataframes, ignore_index=True)
         merged_df.to_csv(output_filename, index=False)
@@ -48,23 +63,26 @@ merge_csv_files(folder_path, output_filename)
 #   Data Cleaning actions
 # =========================
 
-# Load the merged CSV file into a pandas DataFrame
-df = pd.read_csv(folder_path + "MindwaveDB.csv")
+def db_preproccesing():
+    # Load the merged CSV file into a pandas DataFrame
+    df = pd.read_csv(folder_path + "MindwaveDB.csv")
 
-# Remove rows where 'amount_of_noise' is greater than 0, 
-# and where 'meditation' and 'attention' have a value of 0. Resets the df index.
-df = df[(df['amount_of_noise'] == 0) & 
-        (df['meditation'] > 0) & 
-        (df['attention'] > 0)].reset_index(drop=True)
+    # Remove rows where 'amount_of_noise' is greater than 0, 
+    # and where 'meditation' and 'attention' have a value of 0. Resets the df index.
+    df = df[(df['amount_of_noise'] == 0) & 
+            (df['meditation'] > 0) & 
+            (df['attention'] > 0)].reset_index(drop=True)
 
-# Drop 'blink', 'date_time' and 'amount_of_noise' columns
-df = df.drop(['date_time', 'blink', 'amount_of_noise'], axis=1, errors='ignore')
+    # Drop 'blink', 'date_time' and 'amount_of_noise' columns
+    df = df.drop(['date_time', 'blink', 'amount_of_noise'], axis=1, errors='ignore')
+    return df
 
 
 # ==================================
 #   Export dataframe to a CSV file
 # ==================================
 
+df = db_preproccesing()
 # df.to_csv(output_filename, index=False)
 
 # Now 'df' contains the processed data
